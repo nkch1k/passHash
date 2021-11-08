94a7067e4db70003fd963f290baa340085b10dfc
Naumchik <78907633+nkch1k@users.noreply.github.com>
2021-11-08 16:07:18 +0200

    Add files via upload
    work only with   <a  href="https://github.com/kelektiv/node.bcrypt.js/"> Bcrypt</a> and <a  href="https://github.com/expressjs/express"> express</a>

---
+++ serv.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const bcrypt = require('bcrypt')
+const app = express()
+app.use(express.json())
+const users = []
+app.get('/users', (req, res) => {
+  res.json(users)
+})
+
+app.post('/users', async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10)
+    const user = { name: req.body.name, password: hashedPassword }
+    users.push(user)
+    res.status(201).send()
+  } catch {
+    res.status(500).send()
+  }
+})
+
+app.listen(3000)
